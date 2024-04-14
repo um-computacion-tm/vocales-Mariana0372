e25319051c81558f86d6e8c37828adf3847f058a
Mariana0372 <165341564+Mariana0372@users.noreply.github.com>
2024-04-14 20:29:25 -0300

    Update Cadena de frase.py

--- Cadena de frase.py
+++ Cadena de frase.py
@@ -1,10 +1,13 @@
 
 
-texto = input("ingresa una frase: ")
-vocales = "AEIOU"
-j = 0
-for i in texto:
-    if i in vocales:
-        j = j + 1
 
-print(f"la frase tiene {j} vocales")
+def obtener_vocales(frase):
+    vocales = "AEIOU"
+
+    return set([c for c in frase if c in vocales])
+    
+
+texto = input("ingresa una frase: ")
+
+print(obtener_vocales(frase))
+print(len(obtener_vocales(texto)))
